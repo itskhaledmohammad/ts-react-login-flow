da28e5a37a7316222d92e56eb029c0f8deb80aa4
Khaled Mohammad <itskhaledmohammad@gmail.com>
2021-05-04 16:53:27 +0600

    Added React Router.

--- src/App.tsx
+++ src/App.tsx
@@ -18,19 +18,7 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Text>Hello World</Text>
         </VStack>
       </Grid>
     </Box>
