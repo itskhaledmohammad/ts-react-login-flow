d585544bcc52438a524cc2f84614a1bae032453b
Khaled Mohammad <itskhaledmohammad@gmail.com>
2021-05-05 07:02:22 +0600

    Added some states for keeping track of different fields.

--- src/signup/Signup.tsx
+++ src/signup/Signup.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Flex, Text, Button, Icon, useToast, Input, Link} from "@chakra-ui/react";
+import { Flex, Text, Button, Icon, useToast, Input, Link, InputGroup, InputLeftElement} from "@chakra-ui/react";
 import { PhoneIcon } from '@chakra-ui/icons';
 import { FaArrowAltCircleLeft, FaEnvelope } from 'react-icons/fa';
 import {Link as RLink} from "react-router-dom";
@@ -19,10 +19,15 @@ const blackButtonStyle = {
 }
 export const Signup = ():JSX.Element => {
     const toast = useToast()
-    const [otp, setOTP] = useState<number>(0);
-    
+    const [phone, setPhone] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+
+
     const handleOTP = () => {
-        if(otp === 0 || !otp) {
+        if(!phone) {
             toast({
                 title: "Validation Unsuccessful",
                 description: "Please enter a valid OTP",
@@ -36,6 +41,15 @@ export const Signup = ():JSX.Element => {
     return (
     <Flex direction="column" width="100%" padding="50px" justifyContent="center" alignItems="center">
         <Text fontSize="4xl" align="left" width="100%" as="strong">Create a new account</Text>
+        <InputGroup {...commonStyle}>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<PhoneIcon color="gray.500" />}
+              height="100%"
+            />
+            <Input type="tel" placeholder="Phone Number (Required)" paddingTop="30px" paddingBottom="30px" fontSize="2xl" borderRadius="0" borderColor="black" onChange={(e) => setPhone(e.target.value)}/> 
+    </InputGroup>
+
         <Button {...commonStyle} size="lg" {...blackButtonStyle}  padding="30px 0" onClick={handleOTP}>
             <Text fontSize="2xl">Sign Up</Text>
         </Button>
