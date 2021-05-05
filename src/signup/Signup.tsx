5c7eb67e0eb016c8064f1c1927eb096c497aa5cc
Khaled Mohammad <itskhaledmohammad@gmail.com>
2021-05-05 06:55:50 +0600

    Added some buttons and texts to signup.

--- src/signup/Signup.tsx
+++ src/signup/Signup.tsx
@@ -1,7 +1,46 @@
-import React from "react"
-import {Text} from "@chakra-ui/react"
-const Signup = ():JSX.Element => (
-    <Text>Signup</Text>
-)
+import React, { useState } from 'react';
+import { Flex, Text, Button, Icon, useToast, Input, Link} from "@chakra-ui/react";
+import { PhoneIcon } from '@chakra-ui/icons';
+import { FaArrowAltCircleLeft, FaEnvelope } from 'react-icons/fa';
+import {Link as RLink} from "react-router-dom";
+
+const commonStyle = {
+    width: "100%",
+    margin: "10px 0"
+}
+const blackButtonStyle = {
+    backgroundColor: "black",
+    color: "white",
+    borderRadius: "0",
+    _hover: {
+        backgroundColor: "gray.900"
+        
+    }
+}
+export const Signup = ():JSX.Element => {
+    const toast = useToast()
+    const [otp, setOTP] = useState<number>(0);
+    
+    const handleOTP = () => {
+        if(otp === 0 || !otp) {
+            toast({
+                title: "Validation Unsuccessful",
+                description: "Please enter a valid OTP",
+                status: "error",
+                duration: 1000,
+                isClosable: true,
+                position: "top"
+              })            
+        }
+    }
+    return (
+    <Flex direction="column" width="100%" padding="50px" justifyContent="center" alignItems="center">
+        <Text fontSize="4xl" align="left" width="100%" as="strong">Create a new account</Text>
+        <Button {...commonStyle} size="lg" {...blackButtonStyle}  padding="30px 0" onClick={handleOTP}>
+            <Text fontSize="2xl">Sign Up</Text>
+        </Button>
+        <Link fontSize="xl"  width="100%" as={RLink} to="/login" marginTop="10px"><Icon as={FaArrowAltCircleLeft} /> <Text as="strong">Go Back to Sign In</Text></Link>
+    </Flex  > 
+)}
 
 export default Signup;
